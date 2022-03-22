1974a16ff961aa4ece70170ecbf005e1e9ca0596
Abhinav Omprakash <abhinav@teamohana.com>
2022-03-22 14:55:18 +0530

    chore: format sql using pgFormatter

--- sql/ll_lookup_validation_function.sql
+++ sql/ll_lookup_validation_function.sql
@@ -1,12 +1,14 @@
-create or replace function bitemporal_internal.ll_lookup_validation_function(
-p_schema_name text,
-p_table_name text,
-p_column_name text)
-returns boolean immutable as
- $$select count(*)=1 from 
-        pg_proc p 
-     join pg_namespace n ON n.oid = p.pronamespace 
-     and proname ='validate_bt_'||p_table_name||'_'||p_column_name 
-     and n.nspname=p_schema_name
-     ;$$
-language sql;
+CREATE OR REPLACE FUNCTION bitemporal_internal.ll_lookup_validation_function (p_schema_name text, p_table_name text, p_column_name text)
+    RETURNS boolean IMMUTABLE
+    AS $$
+    SELECT
+        count(*) = 1
+    FROM
+        pg_proc p
+        JOIN pg_namespace n ON n.oid = p.pronamespace
+            AND proname = 'validate_bt_' || p_table_name || '_' || p_column_name
+            AND n.nspname = p_schema_name;
+
+$$
+LANGUAGE sql;
+
